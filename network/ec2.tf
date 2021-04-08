93290804a9e3d7b79b35f9a057ebc77424174a49
sondos.medhat <sondos.alagmawy@gmail.com>
2021-04-08 10:25:09 +0200

    inventory file

--- network/ec2.tf
+++ network/ec2.tf
@@ -1,26 +1,26 @@
-# resource "aws_instance" "bastion" {
-#   ami                    = var.awsinstance
-#   instance_type          = "t2.micro"
-#   vpc_security_group_ids = [aws_security_group.allowssh.id]
-#   subnet_id              = aws_subnet.public1.id
-#   key_name               = aws_key_pair.sshpublickey1.key_name
-#   tags = {
-#     Name = "Bastion EC2"
-#   }
+resource "aws_instance" "bastion" {
+  ami                    = var.awsinstance
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.allowssh.id]
+  subnet_id              = aws_subnet.public1.id
+  key_name               = aws_key_pair.sshpublickey1.key_name
+  tags = {
+    Name = "Bastion EC2"
+  }
 
-#   provisioner "local-exec" {
-#     command = "echo the public ip is ${self.public_ip}"
-#   }
+  provisioner "local-exec" {
+    command = "echo ${self.public_ip} >> inventory"
+  }
 
-# }
-
-resource "aws_instance" "application" {
-     ami = var.awsinstance
-    instance_type = "t2.micro"
-    vpc_security_group_ids = [aws_security_group.allowsshand3000.id]
-    subnet_id = aws_subnet.private1.id
-    key_name = aws_key_pair.sshpublickey2.key_name
-    tags = {
-        Name = "Application EC2"
-    }
 }
+
+# resource "aws_instance" "application" {
+#      ami = var.awsinstance
+#     instance_type = "t2.micro"
+#     vpc_security_group_ids = [aws_security_group.allowsshand3000.id]
+#     subnet_id = aws_subnet.private1.id
+#     key_name = aws_key_pair.sshpublickey2.key_name
+#     tags = {
+#         Name = "Application EC2"
+#     }
+# }
