78a6dcff4cc1e61e61a686e1da420f9b594b2301
sondos.medhat <sondos.alagmawy@gmail.com>
2021-04-30 05:56:48 +0200

    new s3

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "s3bucketiti"
+    bucket = "s3bucketiti1"
     key    = "iti/terraform/terraform.tfstate"
     region = "us-east-1"
     #dynamodb_table = "dynamodb"
